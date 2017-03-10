87786bd822ad1ea990fb3d4eb756866dc399535a
aendres <aendres19@gmail.com>
2017-03-10 12:37:54 -0500

    add program for inverting an inputted number

--- InvertedNumbers2/Program.cs
+++ InvertedNumbers2/Program.cs
@@ -10,6 +10,23 @@ namespace InvertedNumbers2
     {
         static void Main(string[] args)
         {
+            string input;
+            string result = "";
+            // created a string for the number entered and for the resulting inverted number
+            Console.WriteLine("Enter your number here: ");
+            input = Console.ReadLine();
+            // the string input is now  eqaul to the number entered 
+
+            foreach (char num in input.ToArray())
+            // created a foreach loop that will take each number one by one starting with the first number entered
+            {
+                result = num + result;
+                // the result in now equal to the inverted number created by the foreach loop
+            }
+
+            Console.WriteLine("Reverse of your number: " + result);
+            Console.ReadLine();
         }
+
     }
 }
